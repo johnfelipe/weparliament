ad129aae10b8b1002e550fa27268204d28fedc84
tbarak2 <barakroy@yahoo.com>
2015-01-27 20:30:40 +0200

    see code review comments: Hide the bill’s details (right part) when no bill is selected. After bill is approved it’s moved to Bill-Approved but it need to be removed from Bill-Draft. Disable deny button if deny reason is empty. Deleted billCategory parameter used  $scope.currentBill

--- app/scripts/controllers/committee.js
+++ app/scripts/controllers/committee.js
@@ -6,30 +6,28 @@
 app.controller('CommitteeCtrl',function($scope,$location,BillDraft,Category,Bill){
   $scope.bills = BillDraft.all();
   var currentBill;
-  $scope.pull = function(billCategory,bill) {
-    $scope.category  = Category.get(billCategory);
-    $scope.title = bill.Title;
-    $scope.description = bill.Description;
+  $scope.pull = function(bill) {
+    currentBill = bill;
+    $scope.category  = Category.get(bill.Category);
+    $scope.currentBill = currentBill;
     bill.status = "InProgress";
     currentBill = bill;
-
 };
+
   $scope.pullBack = function(){
-    //var bill = BillDraft.get(currentBill.$id);
     currentBill.status="Waiting";
     clearScope();
   };
+
   $scope.submitApproveBill=function(){
     currentBill.status = 'Approved';
     Bill.create(currentBill).then(function (ref) {
       $location.path('/main');});
+    BillDraft.remove(currentBill);
   };
 
-  function clearScope()
-  {
-    $scope.category  = null;
-    $scope.title = null;
-    $scope.description = null;
+  function clearScope(){
+    $scope.currentBill = null;
   }
 
 });
