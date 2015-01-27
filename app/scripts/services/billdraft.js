b32bd4ac27d5bf77f403607829b9f27dc5f458f0
tbarak2 <barakroy@yahoo.com>
2015-01-27 22:45:23 +0200

    Used query to pull list of draft bills that no one touch them. Problem with pull back update is not working.

--- app/scripts/services/billdraft.js
+++ app/scripts/services/billdraft.js
@@ -3,6 +3,7 @@
 app.factory('BillDraft', function (FIREBASE_URL, $firebase) {
   var ref = new Firebase(FIREBASE_URL);
   var draftBills = $firebase(ref.child('Bill-Draft')).$asArray();
+  var unhandeledDraftBills =  $firebase(ref.child('Bill-Draft').orderByChild('user').startAt("").endAt("")).$asArray();
 
   var BillDraft = {
     create: function (bill) {
@@ -11,13 +12,24 @@ app.factory('BillDraft', function (FIREBASE_URL, $firebase) {
     all: function () {
       return draftBills;
     },
+    allUnHandled:function(){
+      return unhandeledDraftBills;
+    },
     get: function (billId) {
       var result = $firebase(ref.child('Bill-Draft').child(billId)).$asObject();
       return result;
     },
-	update: function (bill) {
-		return draftBills.$save(bill);
-	},
+    updateUnhandled:function (bill)    {
+      return unhandeledDraftBills.$save(bill)
+    },
+    update: function (bill) {
+      return draftBills.$save(bill);
+    },
+    updatePollBack:function (bill)    {
+      var ref = $firebase(ref.child('Bill-Draft'));
+      ref.set(bill);
+      return unhandeledDraftBills;
+    },
     remove: function (bill) {
       return draftBills.$remove(bill);
     }
