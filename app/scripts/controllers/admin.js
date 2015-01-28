d50e097ea807bed09b165bbf5ba42f8f4f7b0a05
tbarak2 <barakroy@yahoo.com>
2015-01-28 22:12:38 +0200

    Remove all things that belong to status include the status

--- app/scripts/controllers/admin.js
+++ app/scripts/controllers/admin.js
@@ -1,11 +1,10 @@
 'use strict';
 
-app.controller('AdminCtrl', function ($scope, Category,Status) {
+app.controller('AdminCtrl', function ($scope, Category) {
   var renameCategories = {};
   var renameStatus = {};
   $scope.categories = Category.all;
-  $scope.statuses = Status.all;
-
+  
   $scope.addCategory = function () {
     Category.create($scope.newCategory);
     $scope.newCategory = '';
@@ -27,28 +26,4 @@ app.controller('AdminCtrl', function ($scope, Category,Status) {
   $scope.deleteCategory = function (category) {
     Category.delete(category);
   };
-
-  //status//
-  $scope.addStatus = function () {
-    Status.create($scope.newStatus);
-    $scope.newStatus = '';
-  };
-
-  $scope.showRenameStatus = function (status) {
-    renameStatus[status.$id] = !renameStatus[status.$id];
-  };
-
-  $scope.isRenameStatus = function (status) {
-    return renameStatus[status.$id];
-  };
-
-  $scope.renameStatus = function (status) {
-    Status.update(status);
-    renameStatus[status.$id] = false;
-  };
-
-  $scope.deleteStatus = function (status) {
-    Status.delete(status);
-  }
-
 });
