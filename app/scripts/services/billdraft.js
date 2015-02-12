cfe5fbb32f85372e4d07ec66d38ccdfa3d6b31fd
nyccyn <nyccyn@gmail.com>
2015-02-12 18:58:03 +0200

    fix committee

--- app/scripts/services/billdraft.js
+++ app/scripts/services/billdraft.js
@@ -2,28 +2,24 @@
 
 app.factory('BillDraft', function (FIREBASE_URL, $firebase) {
   var ref = new Firebase(FIREBASE_URL);
-  var draftBills = $firebase(ref.child('Bill-Draft')).$asArray();
   var unhandeledDraftBills =  $firebase(ref.child('Bill-Draft').orderByChild('HandledBy').startAt("").endAt("")).$asArray();
 
   var BillDraft = {
     create: function (bill) {
       bill.CreateDate = Firebase.ServerValue.TIMESTAMP;
-      return draftBills.$add(bill);
-    },
-    all: function () {
-      return draftBills;
+      return $firebase(ref.child('Bill-Draft')).$push(bill);
     },
     allUnHandled:function(){
       return unhandeledDraftBills;
     },
     get: function (billId) {
-      return draftBills.$getRecord(billId);
+      return $firebase(ref.child('Bill-Draft').child(billId)).$asObject();
     },
     update: function (bill) {
-      return draftBills.$save(bill);
+      return $firebase(ref.child('Bill-Draft').child(bill.$id)).$set("HandledBy", bill.HandledBy);
     },
     remove: function (bill) {
-      return draftBills.$remove(bill);
+      return $firebase(ref.child('Bill-Draft')).$remove(bill.$id);
     },
     handeledByUser:function(userId){
       return $firebase(ref.child('Bill-Draft').orderByChild('HandledBy').startAt(userId).endAt(userId)).$asArray();
